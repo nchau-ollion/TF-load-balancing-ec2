2d73eb53a7c8ed51241062fb42c5ef84f13e1ded
nchau-ollion <nicole.chau@ollion.com>
2024-02-02 15:10:10 -0600

    Add LB & remove state

--- main.tf
+++ main.tf
@@ -62,12 +62,39 @@ resource "aws_lb" "app_lb" {
   internal            = false
   load_balancer_type  = "application"
   security_groups     = [aws_security_group.for_lb.id]
-  #do I need a lb in each public subnet?
-  subnets             = [aws_subnet.public_subnet.id]
+  subnets             = module.vpc.public_subnets
 }
 
 resource "aws_lb_target_group" "target_group" {
   name = "web-target-group"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = module.vpc.vpc_id
+  health_check {
+    path = "/"
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 5
+    interval = 10
+    matcher = "200-299"
+  }
+}
+
+resource "aws_lb_listener" "lb_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
+
+#register instance to target group
+resource "aws_lb_target_group_attachment" "web_server_attach" {
+  target_group_arn = aws_lb_target_group.target_group.arn
+  target_id        = aws_instance.web_server.id
+  port             = 80
 }
 
 resource "aws_instance" "web_server" {
